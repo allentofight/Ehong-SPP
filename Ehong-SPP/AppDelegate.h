1b1f40475e02827df2e760d32a373170d993b272
xiekun <xiekun605746@163.com>
2015-11-10 18:53:29 +0800

    Initial Commit

---
+++ Ehong-SPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ehong-SPP
+//
+//  Created by ronaldo on 11/10/15.
+//  Copyright © 2015 ronaldo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
